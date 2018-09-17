c91eea6850f63e0cd48f12124c310c3c71e7d783
Laurence <laurence.martinez14@gmail.com>
2018-09-16 23:02:01 -0500

    HTML

---
+++ assets/gameCode.js
@@ -0,0 +1,15 @@
+$(function () {
+    //initialize firebase
+    var config = {
+        apiKey: "AIzaSyCXJ6259qG8CJXHldibJSwcuR59TqiBKyg",
+        authDomain: "laurence-ut-coding-ttr.firebaseapp.com",
+        databaseURL: "https://laurence-ut-coding-ttr.firebaseio.com",
+        projectId: "laurence-ut-coding-ttr",
+        storageBucket: "laurence-ut-coding-ttr.appspot.com",
+        messagingSenderId: "375533271494"
+    };
+    firebase.initializeApp(config);
+
+    
+});
+
