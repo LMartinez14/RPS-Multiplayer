37a221613c005f422b90cfdb99218478f33be303
Laurence <laurence.martinez14@gmail.com>
2018-09-18 18:24:22 -0500

    final commit

--- assets/gameCode.js
+++ assets/gameCode.js
@@ -36,7 +36,7 @@ let playerTwoEntered = false;
 let resetId;
 
 
-// add new player when "login" button is clicked
+// append name to new player when "login" button is clicked
 playersRef.on("value", function (snapshot) {
     // update the names
     $("#playerOne").text(playerOneObject.name = "Awaiting first Player");
@@ -48,16 +48,16 @@ playersRef.on("value", function (snapshot) {
 
     // un-hide/hide "screens" depending on player login
     if (player1LoggedIn && player2LoggedIn && !playerNumber) {
-        playersLogginPending();
+        playersLoginPending();
     } else if (playerNumber) {
         playersAreLoggedIn();
     } else {
-        playersNeedToLogIn ()();
+        playersNeedToLogIn()();
     }
 
-    // if both players have selected their choice, perform the comparison
+    // when both player choose selections, compare, and display
     if (player1Object.choice && player2Object.choice) {
-        rps(player1Object.choice, player2Object.choice);
+        rpsGame(player1Object.choice, player2Object.choice);
     }
 
 });
@@ -65,7 +65,7 @@ playersRef.on("value", function (snapshot) {
 $("#login").click(function (loginEvent) {
     loginEvent.preventDefault();
 
-    // check for availability on load
+    // check for space on loading screen
     if (!player1LoggedIn) {
         playerNumber = "1";
         playerObject = playerOneObject;
@@ -93,9 +93,59 @@ $("#login").click(function (loginEvent) {
     }
 });
 
-// toggle between hidden and shown displays
-function playersLogginPending()
-function playersAreLoggedIn ()
-function playersNeedToLogIn ()
+// game function
+function rpsGame(marioChoice, luigiChoice) {
+    $(".playerOneShowChoice").text(marioChoice);
+    $(".playerTwoShowChoice").text(luigiChoice);
+
+    showSelections();
+
+    // tie
+    if (marioChoice === luigiChoice) {
+        $("#gameResult").text("Play again.");
+    }
+    //compare the selections
+    else if (((marioChoice === "paper" && luigiChoice === "rock") || (marioChoice === "scissors" && luigiChoice === "paper") || marioChoice === "rock" && luigiChoice === "scissors")) {
+        // player 1, mario, wins
+        $("#gameResult").html("<small>" + marioChoice + " beats " + luigiChoice + "</small><br/><br/>" + player1Object.name + " wins!");
+        if (playerNumber === "1") {
+            playerObject.wins++;
+        } else {
+            playerObject.losses++;
+        }
+    } else {
+        // player 2, luigi, wins
+        $("#gameResult").html("<small>" + luigiChoice + " beats " + marioChoice + "</small><br/><br/>" + player2Object.name + " wins!");
+        if (playerNumber === "2") {
+            playerObject.wins++;
+        } else {
+            playerObject.losses++;
+        }
+    }
+    // reset the game
+    resetId = setTimeout(reset, 2000);
+};
+
+// toggle between what is hidden and shown on display
+function playersLoginPending()
+$(".waitingRoom", ".credentials", ".choices").hide();
+$(".roomFull").show();
+function playersNeedToLogIn()
+$(".waitingRoom", ".roomFull", ".credentials", ".choices").hide();
+$(".").show();
+function playersAreLoggedIn()
+$(".waitingRoom", ".roomFull", ).hide();
+$(".credentials").show();
+if (playerNumber === "1") {
+    $(".playerOneChoices").show();
+} else {
+    $(".playerOneChoices").hide();
+}
+if (playerNumber === "2") {
+    $(".playerTwoStats").show();
+} else {
+    $(".playerTwoStats").hide();
+};
+
 
 
